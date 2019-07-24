d6c8335cb86f33df6501c3bd3a333269ce139b9a
birna <briandam26@yahoo.com>
2019-07-23 22:32:05 -0700

    Updates

---
+++ precache-manifest.8064fcc503a6bad2f89f06c09d6f2ccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27306da1f4987477e7cb2b3d1cc74867",
+    "url": "/clickyGame/my-app/index.html"
+  },
+  {
+    "revision": "60c2743281699d858bbb",
+    "url": "/clickyGame/my-app/static/css/main.69ba837f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "60c2743281699d858bbb",
+    "url": "/clickyGame/my-app/static/js/main.06a0fb1a.chunk.js"
+  },
+  {
+    "revision": "1d8d26a6a1661a515c9c",
+    "url": "/clickyGame/my-app/static/js/runtime~main.3b565f7b.js"
+  }
+]);
